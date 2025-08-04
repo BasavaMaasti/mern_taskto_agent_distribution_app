5ce42e590e3ea258b5d0744ba9ffc08e74b62e69
BasavaMaasti <143349758+BasavaMaasti@users.noreply.github.com>
2025-08-04 17:46:15 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
+const BASE_URL = 'https://mern-taskto-agent-distribution-app.onrender.com/api';
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
